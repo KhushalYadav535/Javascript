f25eed14f063db08d1977602b1aaa946fd95eb70
Khushal Yadav <92143510+KhushalYadav535@users.noreply.github.com>
2024-01-09 19:58:32 +0000

    datatypes

---
+++ 01_basics/datatype.js
@@ -0,0 +1,14 @@
+//"use strict" // code will be treat as a newer version of javascript
+// alert("Hello") we are using node js not browser
+let name="Khushal";
+let age=21;
+let isLoggedin=false;
+let state=null;
+// int
+// bigint
+// string
+// boolean
+// null stand alone value its type is object
+// undefined only variable define not the value will be assign
+
+console.log(typeof null)
