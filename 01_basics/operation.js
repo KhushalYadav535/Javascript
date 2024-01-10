98630d082b5233d9740da0e82c7174dca058571a
Khushal Yadav <92143510+KhushalYadav535@users.noreply.github.com>
2024-01-10 12:40:02 +0000

    update operation

--- 01_basics/operation.js
+++ 01_basics/operation.js
@@ -5,3 +5,20 @@ let value=Number(score);
 console.log(value);
 console.log(typeof (value));
 console.log(value);
+// ********** operation ***********
+
+
+console.log( 2 + 2);
+console.log( 2-2);
+console.log(2**3);
+console.log(2%3);
+console.log(2/3);
+
+let str1="hello"
+let str2= " khushal"
+let str3= str1+str2
+console.log(str3)
+console.log(1 + "2");
+console.log("1"+2);
+console.log("1"+"2"+2);
+console.log(1+2+"2");
